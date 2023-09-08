8cf862aaccfe174d3988516020b9380f4c43a1c9
Charlie Kozey <charkoz@mailbox.org>
2023-09-08 13:28:57 -0400

    improve time and space complexity of valid palindromes; update strategies

--- strategies.md
+++ strategies.md
@@ -1,5 +1,39 @@
 ## Strategies and Patterns
 
 ### Iterate once while keeping track of global max/mins
+Rather than comparing each element to every other element, keep track of relevant max/mins as you go
+- Benefits:
+    - Saves time compared to nested loops
 - Used in:
     - Best Time to Buy and Sell Stock
+
+### Two pointer
+Can be for loop with two variables:
+```js
+for (let i=0, j=arr.length-1; i <= j; i++, j--){
+    // ...
+}
+```
+Or while loop with left/right:
+```js
+let left = 0
+let right = 0
+
+while (left <= right) {
+    // ...
+    left ++
+    right --
+}
+```
+- Benefits:
+    - Saves space compared to creating a new array to track values
+- Used in:
+    - Valid Palindromes
+
+### Edit input in place
+Can use regex, .replace() (or equivalent)
+
+- Benefits:
+    - Saves time and space compared to creating a new entity
+- Used in:
+    - Valid Palindromes
--- valid-palindromes/index.js
+++ valid-palindromes/index.js
@@ -19,22 +19,21 @@
 // 6. join filteredArray into simplifiedString
 // 7. return simplifiedString == reversedString
 
-var isPalindrome = function(string) {
-    const alphanumericChars = "qwertyuiopasdfghjklzxcvbnm1234567890"
-    const stringToArray = string.toLowerCase().split("")
-    const filteredArray = stringToArray.filter(char => alphanumericChars.includes(char))
-    const simplifiedString = filteredArray.join("")
-    let reversedString = ""
+// var isPalindrome = function(string) {
+//     const alphanumericChars = "qwertyuiopasdfghjklzxcvbnm1234567890"
+//     const stringToArray = string.toLowerCase().split("")
+//     const filteredArray = stringToArray.filter(char => alphanumericChars.includes(char))
+//     const simplifiedString = filteredArray.join("")
+//     let reversedString = ""
 
-    for (let i=0; i < filteredArray.length; i++) {
-        reversedString = filteredArray[i] + reversedString
-    }
+//     for (let i=0; i < filteredArray.length; i++) {
+//         reversedString = filteredArray[i] + reversedString
+//     }
 
-    return reversedString == simplifiedString
-    
-};
+//     return reversedString == simplifiedString
+// };
 
-console.log(isPalindrome(""))
+// console.log(isPalindrome(""))
 
 // ok this works
 
@@ -44,3 +43,34 @@ console.log(isPalindrome(""))
 // Space Complexity: (I *think*) O(n) because there are 4 variables that depend on the
 // input. They all scale linearly with length of input string. For extremely large
 // values of n, 4n should reduce to n
+
+////// IMPROVED SOLUTION ///////////////////////////
+
+// I think time complexity of O(n) is okay, but can definitely improve space complexity.
+// After some research, using two pointer approach seems to be solid.
+// It avoids having to store characters in memory in strings/arrays.
+
+// Can use a while loop with left and right, or a for loop with two variables
+
+var isPalindrome = function(string) {
+    string = string.toLowerCase()
+    string = string.replace(/[^0-9a-z]/gi, "")
+    let left = 0
+    let right = string.length - 1
+
+    while (left < right) {
+        if (string[left] !== string[right]) {
+            return false
+        } else {
+            left ++
+            right --
+        }
+    }
+
+    return true
+};
+
+// Space: O(1) because we're not storing any new items in memory
+// Time: O(n/2) because we're iterating for half the length of the string at most
+
+console.log(isPalindrome(""))
